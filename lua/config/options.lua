c2b7108b01d92767eb99ba40dbd2d2f56dbe6f3d
tmcuong <cuong20022006@gmail.com>
2026-09-21 23:47:55 +0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -43,9 +43,9 @@ opt.listchars = {
 }
 
 opt.expandtab = true
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.softtabstop = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
 opt.autoindent = true
 opt.smartindent = true
 
